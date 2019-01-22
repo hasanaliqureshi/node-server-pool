360934fb71bf3a353033ef788fa200c85611a550
hasanaliqureshi <hasanali.zeds@gmail.com>
2019-01-22 16:09:24 +0500

    cron started

--- server/helpers/rewardDistribution.js
+++ server/helpers/rewardDistribution.js
@@ -23,7 +23,6 @@ const hashCalc = hash => {
 const giveReward = () => {
 	hashSchema.find({is_rewarded : false}).then(docs => {
 		docs.map(doc => {
-			if(doc.last_updated){
 				console.log(doc.last_updated);
 				let md = new Date(doc.last_updated);
 				let cd = new Date(Date.now());
@@ -31,41 +30,57 @@ const giveReward = () => {
 				console.log('----------------------------------');
 				console.log(`${md} < ${cd} is validated ${md < cd}`);
 				if(md < cd) {
-					doc.hash.map(hash => {
-						var hashDetails = hash.toObject();
-						let totalHash = hashDetails.totalHash;
-						let totalHashRate = hashDetails.hashRate;
-						let difficulty;
-						if (doc.difficulty < 50) {
-							difficulty = 100 - doc.difficulty;
-						}else {
-							difficulty = doc.difficulty;	
-						}
-						let reward;
-						if (totalHash == 0 || totalHashRate == 0 || difficulty == 0){
-							reward = 0;
-						}else{
-							reward = ((hashCalc(totalHash) / (hashCalc(totalHash) + totalHashRate)) / difficulty).toFixed(3);
-						}
-						console.log(`(${totalHash} / ${totalHashRate}) / ${difficulty} == ${reward}`);
-						console.log('----------------------------------');
-						hashSchema.findOneAndUpdate({_id : doc._id}, {'is_rewarded' : true, 'total_reward' : reward}, {new: true}).then(doc=> {
-							request.post({
-							  headers: {'content-type' : 'application/x-www-form-urlencoded'},
-							  url:     'https://streemie.com/appv2/api',
-							  body:    "update_hash=true&hash_id="+doc._id+"&hash="+doc.hash.totalHash+"&reward="+doc.total_reward+"&userid="+doc.userid
-							}, function(error, response, body){
-								if(error){
-									console.log(error);
-								}
-							  console.log(body);
+					if(doc.last_updated){
+						doc.hash.map(hash => {
+							var hashDetails = hash.toObject();
+							let totalHash = hashDetails.totalHash;
+							let totalHashRate = hashDetails.hashRate;
+							let difficulty;
+							if (doc.difficulty < 50) {
+								difficulty = 100 - doc.difficulty;
+							}else {
+								difficulty = doc.difficulty;	
+							}
+							let reward;
+							if (totalHash == 0 || totalHashRate == 0 || difficulty == 0){
+								reward = 0;
+							}else{
+								reward = ((hashCalc(totalHash) / (hashCalc(totalHash) + totalHashRate)) / difficulty).toFixed(3);
+							}
+							console.log(`(${totalHash} / ${totalHashRate}) / ${difficulty} == ${reward}`);
+							console.log('----------------------------------');
+							hashSchema.findOneAndUpdate({_id : doc._id}, {'is_rewarded' : true, 'total_reward' : reward}, {new: true}).then(doc=> {
+								request.post({
+								  headers: {'content-type' : 'application/x-www-form-urlencoded'},
+								  url:     'https://streemie.com/appv2/api',
+								  body:    "update_hash=true&hash_id="+doc._id+"&hash="+doc.hash.totalHash+"&reward="+doc.total_reward+"&userid="+doc.userid
+								}, function(error, response, body){
+									if(error){
+										console.log(error);
+									}
+								  console.log(body);
+								});
+							}).catch(err => {
+								console.log(err);
 							});
-						}).catch(err => {
-							console.log(err);
 						});
-					});				
+					}else{
+						hashSchema.findOneAndUpdate({_id : doc._id}, {'is_rewarded' : true, 'total_reward' : 0, 'hash' : {'totalHash' : 0, 'hashRate' : 0}}, {new: true}).then(doc=> {
+								request.post({
+								  headers: {'content-type' : 'application/x-www-form-urlencoded'},
+								  url:     'https://streemie.com/appv2/api',
+								  body:    "update_hash=true&hash_id="+doc._id+"&hash="+doc.hash.totalHash+"&reward="+doc.total_reward+"&userid="+doc.userid
+								}, function(error, response, body){
+									if(error){
+										console.log(error);
+									}
+								  console.log(body);
+								});
+							}).catch(err => {
+								console.log(err);
+							});
+					}			
 				};
-			}
 		});
 	}).catch(err => {
 		console.log(err);
