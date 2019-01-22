28956b88b310ca74d4e4172e4105633de6e3f9cc
hasanaliqureshi <hasanali.zeds@gmail.com>
2019-01-22 16:12:15 +0500

    cron started

--- server/helpers/rewardDistribution.js
+++ server/helpers/rewardDistribution.js
@@ -23,6 +23,7 @@ const hashCalc = hash => {
 const giveReward = () => {
 	hashSchema.find({is_rewarded : false}).then(docs => {
 		docs.map(doc => {
+			if(doc.last_updated){
 				console.log(doc.last_updated);
 				let md = new Date(doc.last_updated);
 				let cd = new Date(Date.now());
@@ -30,7 +31,7 @@ const giveReward = () => {
 				console.log('----------------------------------');
 				console.log(`${md} < ${cd} is validated ${md < cd}`);
 				if(md < cd) {
-					if(doc.last_updated){
+					// if(doc.last_updated){
 						doc.hash.map(hash => {
 							var hashDetails = hash.toObject();
 							let totalHash = hashDetails.totalHash;
@@ -63,24 +64,24 @@ const giveReward = () => {
 							}).catch(err => {
 								console.log(err);
 							});
-						});
-					}else{
-						hashSchema.findOneAndUpdate({_id : doc._id}, {'is_rewarded' : true, 'total_reward' : 0, 'hash' : {'totalHash' : 0, 'hashRate' : 0}}, {new: true}).then(doc=> {
-								request.post({
-								  headers: {'content-type' : 'application/x-www-form-urlencoded'},
-								  url:     'https://streemie.com/appv2/api',
-								  body:    "update_hash=true&hash_id="+doc._id+"&hash="+doc.hash.totalHash+"&reward="+doc.total_reward+"&userid="+doc.userid
-								}, function(error, response, body){
-									if(error){
-										console.log(error);
-									}
-								  console.log(body);
-								});
-							}).catch(err => {
-								console.log(err);
-							});
-					}			
+						});		
 				};
+			}else{
+				hashSchema.findOneAndUpdate({_id : doc._id}, {'is_rewarded' : true, 'total_reward' : 0, 'hash' : {'totalHash' : 0, 'hashRate' : 0}}, {new: true}).then(doc=> {
+						request.post({
+						  headers: {'content-type' : 'application/x-www-form-urlencoded'},
+						  url:     'https://streemie.com/appv2/api',
+						  body:    "update_hash=true&hash_id="+doc._id+"&hash="+doc.hash.totalHash+"&reward="+doc.total_reward+"&userid="+doc.userid
+						}, function(error, response, body){
+							if(error){
+								console.log(error);
+							}
+						  console.log(body);
+						});
+					}).catch(err => {
+						console.log(err);
+					});
+			}	
 		});
 	}).catch(err => {
 		console.log(err);
