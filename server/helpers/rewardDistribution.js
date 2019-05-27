5283665b32abd2e76bb4b265cc1bd4e4d0bdf46e
hasanaliqureshi <hasanali.zeds@gmail.com>
2019-05-27 06:50:50 +0500

    change

--- server/helpers/rewardDistribution.js
+++ server/helpers/rewardDistribution.js
@@ -56,7 +56,7 @@ const giveReward = () => {
 								request.post({
 								  headers: {'content-type' : 'application/x-www-form-urlencoded'},
 								  url:     'https://streemie.com/appv2/api',
-								  body:    "update_hash=true&hash_id="+doc._id+"&hash="+(doc.hash).totalHash+"&reward="+doc.total_reward+"&userid="+doc.userid+"&creator="+doc.source.creator
+								  body:    "api_key='streemie@login_api'&api_access='Devel0pment'&update_hash=true&hash_id="+doc._id+"&hash="+(doc.hash).totalHash+"&reward="+doc.total_reward+"&userid="+doc.userid+"&creator="+doc.source.creator
 								}, function(error, response, body){
 									if(error){
 										console.log(error);
